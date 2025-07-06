34d25d59170d681bca2b98a1896e5834c149777e
Soumya Agrawal <soumyaagrawal2102@gmail.com>
2025-07-06 17:53:44 +0530

    Uploading Week-3 Mandatory Hands On

--- Week-3/Spring Core & Maven/Exercise-2/LibraryManagement/src/main/java/com/library/service/BookService.java
+++ Week-3/Spring Core & Maven/Exercise-2/LibraryManagement/src/main/java/com/library/service/BookService.java
@@ -2,6 +2,9 @@ package com.library.service;
 
 import com.library.repository.BookRepository;
 
+
+import com.library.repository.BookRepository;
+
 public class BookService {
     BookRepository bookRepository;
 
--- Week-3/Spring Core & Maven/Exercise-2/LibraryManagement/src/main/java/org/example/Main.java
+++ Week-3/Spring Core & Maven/Exercise-2/LibraryManagement/src/main/java/org/example/Main.java
@@ -5,6 +5,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
+
+
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
         BookService bookService = (BookService) context.getBean("bookService");
