88e6fd11427d60cadb7de96f8a058a55b62621ab
Soumya Agrawal <soumyaagrawal2102@gmail.com>
2025-07-27 16:31:27 +0530

    Uploading Week-6 Mandatory Hands On

---
+++ week-6/ReactJs-HOL-4/blogapp/src/Posts.js
@@ -0,0 +1,51 @@
+import { Component } from "react";
+import Post from "./Post";
+
+class Posts extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [],
+      hasError: false,
+    };
+  }
+
+  loadPosts = () => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((data) => this.setState({ posts: data }))
+      .catch((error) => {
+        console.error("Error fetching posts:", error);
+        this.setState({ hasError: true });
+      });
+  };
+
+  componentDidMount() {
+    this.loadPosts();
+  }
+
+  componentDidCatch(error, info) {
+    alert("An error occurred in the Posts component.");
+    console.error("Error caught by componentDidCatch:", error, info);
+    this.setState({ hasError: true });
+  }
+
+  render() {
+    const { posts, hasError } = this.state;
+
+    if (hasError) {
+      return <h2>Something went wrong while loading posts.</h2>;
+    }
+
+    return (
+      <div>
+        <h1>Blog Posts</h1>
+        {posts.map((post) => (
+          <Post key={post.id} title={post.title} body={post.body} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Posts;
